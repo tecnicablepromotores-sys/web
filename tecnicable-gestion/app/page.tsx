9bbf156534b6a55b375341c48463ab840fbe2335
tecnicablepromotores-sys <tecnicablepromotores@gmail.com>
2026-07-31 14:44:17 -0400

    Delete tecnicable-gestion directory

--- tecnicable-gestion/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function Page() {
-  // La aplicación Tecnicable Pro es una app HTML autocontenida servida desde /public
-  redirect("/tecnicable.html")
-}
